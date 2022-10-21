9ed9efaaaa71a6a67a92886f50098498297cb566
rttee3 <105558204+rttee3@users.noreply.github.com>
2022-10-21 00:30:00 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-PORT=53854
-UUID=f910131d-4ce1-4eea-b082-3cafc8faa6ca
+PORT=57854
+UUID=2f573d5d-078d-4da6-971a-1f54fd60df33
 WebPage=https://bing.com
-CaddyConfig=https://raw.githubusercontent.com/chaohuxiangxiang/python/main/etc/Caddyfile
-XRayConfig=https://raw.githubusercontent.com/chaohuxiangxiang/python/main/etc/xray.json
+CaddyConfig=https://raw.githubusercontent.com/rttee3/northfland2/main/etc/Caddyfile
+XRayConfig=https://raw.githubusercontent.com/rttee3/northfland2/main/etc/xray.json
 Xray_Newv=`wget --no-check-certificate -qO- https://api.github.com/repos/XTLS/Xray-core/tags | grep 'name' | cut -d\" -f4 | head -1 | cut -b 2-`
 # Install XRay
 mkdir -p /tmp/app
